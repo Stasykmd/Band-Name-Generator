235907769a8ab3c4901cd8ed20e95f03b7f44499
Stasykmd <118196704+Stasykmd@users.noreply.github.com>
2023-11-18 22:29:44 +0200

    Band-Name-Generator
    Band-Name-Generator

---
+++ main.py
@@ -0,0 +1,4 @@
+print('Welcome to the Brand Name Generator.')
+grew_up_in = input("What's the city that you grew up in?\n")
+name_pet = input("What is the name of the pet?\n")
+print("Your band name could be:\n" + grew_up_in + " " + name_pet)
